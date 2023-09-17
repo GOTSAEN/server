2ab5aaaaf7321e1b3761092118b6b4eb76942d85
BEOMSEOK <77001461+sbs0711@users.noreply.github.com>
2023-09-17 18:55:25 +0900

    feat: getMember (#36)

--- src/main/java/com/gotsaen/server/member/dto/MemberUpdateDto.java
+++ src/main/java/com/gotsaen/server/member/dto/MemberUpdateDto.java
@@ -2,22 +2,18 @@ package com.gotsaen.server.member.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @NoArgsConstructor
 @Getter
-@Setter
-public class UpdateMemberDto {
-    @NotBlank
+public class MemberUpdateDto {
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$")
     private String newPassword;
 
-    @NotBlank
     private String newBusinessName;
+
     private String newBusinessAddress;
 
 }
--- src/main/java/com/gotsaen/server/member/mapper/MemberMapper.java
+++ src/main/java/com/gotsaen/server/member/mapper/MemberMapper.java
@@ -1,11 +1,15 @@
 package com.gotsaen.server.member.mapper;
 
 import com.gotsaen.server.member.dto.MemberDto;
+import com.gotsaen.server.member.dto.MemberResponseDto;
+import com.gotsaen.server.member.dto.MemberUpdateDto;
 import com.gotsaen.server.member.entity.Member;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
     Member memberPostToMember(MemberDto.Post requestBody);
+    MemberResponseDto memberToMemberResponse(Member member);
 }
