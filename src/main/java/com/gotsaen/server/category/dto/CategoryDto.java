c8d831abdf69ea72033e648c00509967d6098ed4
him0814 <him1325@gmail.com>
2023-09-20 15:46:57 +0900

    feat: category

---
+++ src/main/java/com/gotsaen/server/category/dto/CategoryDto.java
@@ -0,0 +1,10 @@
+package com.gotsaen.server.category.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CategoryDto {
+    private String categoryName;
+}
---
+++ src/main/java/com/gotsaen/server/category/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.gotsaen.server.category.repository;
+
+import com.gotsaen.server.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByCategoryName(String categoryName);
+}
