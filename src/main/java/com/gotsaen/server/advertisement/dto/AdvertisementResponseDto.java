d562af779f3d0c8037991228ebfd524c55aae3bf
chunbu <beso1234@naver.com>
2023-11-06 17:27:25 +0900

    fix: 광고조회 urlList 수정

--- src/main/java/com/gotsaen/server/advertisement/dto/AdvertisementResponseDto.java
+++ src/main/java/com/gotsaen/server/advertisement/dto/AdvertisementResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
@@ -20,5 +21,5 @@ public class AdvertisementResponseDto {
     private String precaution;
     private Advertisement.Status status;
     @Setter
-    private String imageUrl;
+    private List<String> imageUrls;
 }
